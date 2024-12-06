81f7bd5017a30eb207f0ad15095a9a87eb01d886
Gregory Becker <becker33@llnl.gov>
2024-12-06 09:58:36 -0800

    concretization: move single-spec concretization logic to spack.concretize
    This resolves a circular import issue between spack.spec and spack.concretize.
    It requires removing the `Spec.concretize` and `Spec.concretized` methods and
    updating all call-sites to use `spack.concretize.concretized` instead.
    
    This will help with potential future efforts to separate AbstractSpec and
    ConcreteSpec classes.
    
    New import relationship is `spack.concretize` imports from `spack.spec`, but
    not the other way around.

--- lib/spack/spack/test/ci.py
+++ lib/spack/spack/test/ci.py
@@ -10,6 +10,7 @@ import pytest
 import llnl.util.filesystem as fs
 
 import spack.ci as ci
+import spack.concretize
 import spack.environment as ev
 import spack.error
 import spack.paths as spack_paths
@@ -450,7 +451,7 @@ def test_ci_run_standalone_tests_not_installed_junit(
     log_file = tmp_path / "junit.xml"
     args = {
         "log_file": str(log_file),
-        "job_spec": spack.spec.Spec("printing-package").concretized(),
+        "job_spec": spack.concretize.concretized(spack.spec.Spec("printing-package")),
         "repro_dir": str(repro_dir),
         "fail_fast": True,
     }
@@ -469,7 +470,7 @@ def test_ci_run_standalone_tests_not_installed_cdash(
     log_file = tmp_path / "junit.xml"
     args = {
         "log_file": str(log_file),
-        "job_spec": spack.spec.Spec("printing-package").concretized(),
+        "job_spec": spack.concretize.concretized(spack.spec.Spec("printing-package")),
         "repro_dir": str(repro_dir),
     }
 
@@ -502,7 +503,7 @@ def test_ci_run_standalone_tests_not_installed_cdash(
 def test_ci_skipped_report(tmpdir, mock_packages, config):
     """Test explicit skipping of report as well as CI's 'package' arg."""
     pkg = "trivial-smoke-test"
-    spec = spack.spec.Spec(pkg).concretized()
+    spec = spack.concretize.concretized(spack.spec.Spec(pkg))
     ci_cdash = {
         "url": "file://fake",
         "build-group": "fake-group",
